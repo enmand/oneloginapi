21eca7d0a0cfac4a024f25074f1540df97fa0736
Dan Enman <enmand@gmail.com>
2015-06-07 23:22:55 -0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+import os
+from setuptools import setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name="onelogin",
+    version="0.0.1",
+    author="Daniel Enman",
+    author_email="enmand@gmail.com",
+    description=("An API for interacting with OneLogin"),
+    license="BSD",
+    packages=['onelogin'],
+    long_description=read('README'),
+)
