fbb2e253e9af3fad12fd47a8072726eedb1c9070
Dan Enman <enmand@gmail.com>
2015-06-08 23:41:46 -0300

    Add base OneLogin functionality in package init

---
+++ onelogin/__init__.py
@@ -0,0 +1,45 @@
+import requests
+
+
+API_HOST = "https://api.onelogin.com"
+API_VERS = "/api/v3"
+API_URL = "%s%s" % (API_HOST, API_VERS)
+
+class OneLogin(object):
+    """ OneLogin base class for common API management """
+    def __init__(self, api_key):
+        """ Initialize the OneLogin API, with the given API key
+
+        Parameters:
+            api_key - The API key to use when interacting with OneLogin. See
+                http://developers.onelogin.com/v1.0/docs#section-get-an-api-key
+        """
+        # cache for calls that require listing for identification
+        self._api_key = api_key
+        self._conn = OneLogin.session(api_key)
+
+        return self._conn
+
+    @staticmethod
+    def session(_api_key):
+        """ Create a new requests session for the OneLogin server, with the
+        given API credentials.
+        """
+        r = requests.Session()
+        r.auth = (_api_key, "x")
+
+        return r
+
+class APIObject(object):
+    """ A OneLogin API object
+
+    See also http://developers.onelogin.com
+    """
+    def __init__(self, el):
+        self.__details = el
+
+    def __getattr__(self, key):
+        return self._find(key).text
+
+    def _find(self, key):
+        return self.__details.find(key)
